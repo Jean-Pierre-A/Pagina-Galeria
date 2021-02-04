82298f05a3bb0ba9f4ca3b3e59bbaef2b94bd04d
Jean-Pierre-A <jean.agudelo@upb.edu.co>
2021-02-04 02:54:29 -0500

    Olvido Contrasena

--- js/main.js
+++ js/main.js
@@ -8,8 +8,14 @@ if (a) {
 let b = document.getElementById("btn-login");
 if (b) {
   b.addEventListener('click', inicio);
+}
+let c = document.getElementById("btn-contasena");
+if (c) {
+  c.addEventListener('click', olvido_contrasena);
 
 }
+
+
 function registrar() {
 
   let newUser = [],
@@ -22,7 +28,7 @@ function registrar() {
   Email = document.getElementById("input-contrasena").value;
 
   if (Username != "" && Password != "" && Email != "") {
-    let pass = review(Username, Password);
+    let pass = review(Username, Password , Email, 1);
     if (pass == false) {
       newUser.push(Username, Password, Email);
       addInUserlist(newUser);
@@ -46,15 +52,21 @@ function getUserlist() {
   }
   return LocalUserlist;
 }
-function review(username, password) {
-  alert("Funcion review");
+
+function review(username, password,email,key) {
   let Userlist = getUserlist();
   let pass = false;
-  
 
   for (let i = 0; i < Userlist.length; i++) {
     if (username == Userlist[i][0] && password == Userlist[i][2]) {
+      if(key == 1){
+
+      if(email == Userlist[i][1]){
       pass = true;
+      }
+    }else{
+      pass = true;
+    }
     }
   }
   return pass;
@@ -67,7 +79,7 @@ function inicio() {
   Username = document.getElementById("nombre").value;
   Password = document.getElementById("contrasena").value;
   if (Username != "" && Password != "") {
-    let pass = review(Username, Password);
+    let pass = review(Username, Password, null, 0);
     if (pass == true) {
       window.location.href = "galeria.html";
     } else {
@@ -76,4 +88,33 @@ function inicio() {
   }else{
       alert('Ingrese todos los campos');
   }
+}
+
+function olvido_contrasena(){
+  let Userlist = getUserlist();
+  let Username = "";
+  let Email = "";
+   Username = document.getElementById('nombre-conta').value;
+   Email = document.getElementById('email-conta').value;
+  let Password = "";
+
+  if (Username != "" && Email != "") {
+    
+    for(let i = 0; i<Userlist.length; i++){
+
+      if (Username == Userlist[i][0] && Email == Userlist[i][1]) {
+
+        Password ='es: '+ Userlist[i][2];
+        break;
+      }
+      else{
+        Password ='No se encuentran los datos ingresados, verifique';
+      }
+    }
+    alert('Su contraseña es   '+ Password);
+  }else{
+      alert('Ingrese todos los campos');
+  }
+
+
 }
